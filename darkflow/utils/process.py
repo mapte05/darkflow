1b1f7b3a92d4f2f93e9b373211de796fe895691a
Maneesh Apte <maneesh@kanjoya.com>
2017-06-04 16:39:58 -0700

    added parsing code for new fire module

--- darkflow/utils/process.py
+++ darkflow/utils/process.py
@@ -312,6 +312,21 @@ def cfg_yielder(model, binary):
 			c = c * (stride ** 2)
 			l = w * h * c
 		#-----------------------------------------------------
+		elif d['type'] == '[fire]':
+			s_1x1 = d.get('s_1x1', 16)
+			e_1x1 = d.get('e_1x1', 64)
+			e_3x3 = d.get('e_1x1', 64)
+			n = e_1x1 + e_3x3
+			activation = d.get('activation', 'relu')
+			yield ['fire', i, c, n, e_1x1, e_3x3, activation]
+			if activation != 'linear': yield [activation, i]
+			c = n
+			# don't need to reset width and hidth
+			l = w * h * c
+			print("HOLY WOWW")
+			print("s_1x1: %f, e_1x1: %f, e_3x3: %f" % (s_1x1, e_1x1, e_3x3))
+
+		#-----------------------------------------------------
 		else:
 			exit('Layer {} not implemented'.format(d['type']))
 
