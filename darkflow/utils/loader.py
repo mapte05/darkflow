b12f9d8f5e2cd33915463da8f2a88d18b5a1a94c
Maneesh Apte <maneesh@bsifargo.com>
2017-06-10 14:23:41 -0700

    updated labels to have all voc 2012 classes

--- darkflow/utils/loader.py
+++ darkflow/utils/loader.py
@@ -86,10 +86,7 @@ class checkpoint_loader(loader):
         meta = ckpt + '.meta'
         with tf.Graph().as_default() as graph:
             with tf.Session().as_default() as sess:
-                print("meta: %s" % meta)
                 saver = tf.train.import_meta_graph(meta)
-                print(sess)
-                print(saver)
                 saver.restore(sess, ckpt)
                 for var in tf.global_variables():
                     name = var.name.split(':')[0]
